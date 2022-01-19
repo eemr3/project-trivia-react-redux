5158ba49fecb668e36cf63c8cbc6764f5048fcbb
Emerson <eemr3@yahoo.com.br>
2022-01-19 16:18:46 -0300

    [feat]: cria componente que apresenta o total do score e dos assertions

--- src/pages/FeedBack.js
+++ src/pages/FeedBack.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Message from '../components/Message';
 
 class FeedBack extends Component {
   render() {
+    const { score, assertions } = this.props;
     return (
       <div>
         <Header />
         <h1 data-testid="feedback-text">FeedBack</h1>
         <Message />
+        <section>
+          <h3 data-testid="feedback-total-score">{score}</h3>
+          <p data-testid="feedback-total-question">{assertions}</p>
+        </section>
       </div>
     );
   }
 }
 
-export default FeedBack;
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+export default connect(mapStateToProps)(FeedBack);
